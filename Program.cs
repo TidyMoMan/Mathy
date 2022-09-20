74a2c2b74d31361d4cfc6ee533a27e54bb1070ac
TidyMoMan <77814251+TidyMoMan@users.noreply.github.com>
2022-09-20 11:13:44 -0400

    Added X intercept

--- Program.cs
+++ Program.cs
@@ -1,18 +1,16 @@
 ﻿while (true)
 {
     Console.WriteLine("Input coordinates x1, y2, x2, y2");
-    Console.Write("X1: "); double coordX1 = Convert.ToDouble(Console.ReadLine());
-    Console.Write("Y1: "); double coordY1 = Convert.ToDouble(Console.ReadLine());
-    Console.Write("X2: "); double coordX2 = Convert.ToDouble(Console.ReadLine());
-    Console.Write("Y2: "); double coordY2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Point 1: ("); double coordX1 = Convert.ToDouble(Console.ReadLine()); Console.SetCursorPosition(11, 1); Console.Write(", "); double coordY1 = Convert.ToDouble(Console.ReadLine()); Console.SetCursorPosition(14, 1); Console.WriteLine(")");
+    Console.Write("Point 2: ("); double coordX2 = Convert.ToDouble(Console.ReadLine()); Console.SetCursorPosition(11, 2); Console.Write(", "); double coordY2 = Convert.ToDouble(Console.ReadLine()); Console.SetCursorPosition(14, 2); Console.WriteLine(")");
 
     double run = (coordX2 - coordX1);
     double rise = (coordY2 - coordY1);
     double slope = rise*-1 / run*-1;
     //double angle = Math.Pow(Math.Tan(rise / run), -1);
     double intercept = coordY1 - (slope * coordX1);
-    double Xintercept = (coordY1 * slope) + (intercept * slope);
-    Console.WriteLine(rise * -1 + " <--rise & run--> " + run * -1 + " between points");
+    double Xintercept = intercept / -slope;
+    //Console.WriteLine(rise * -1 + " <--rise & run--> " + run * -1 + " between points");
     Console.WriteLine("slope " + slope);
     //Console.WriteLine("angle of line: " + angle);
     Console.WriteLine("Y intercept: " + intercept);
